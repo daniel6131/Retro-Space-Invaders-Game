38b3cc789f86ac30dd951a83d150d5c9e65c5b50
Daniel O'Brien <975241@swansea.ac.uk>
2022-02-12 16:42:45 +0000

    Invader Movement
    Allowing the invaders to move across the screen, and when an invader's x position is close to either the right or left side the invader grid will go down a row and flip direction.

--- Assets/Scripts/Invaders.cs
+++ Assets/Scripts/Invaders.cs
@@ -8,6 +8,11 @@ public class Invaders : MonoBehaviour
     public int rows = 5;
     public int columns = 11;
 
+    // The speed at which the invaders move
+    public float speed = 1.0f;
+    // The inital direction the invaders move
+    private Vector3 _direction = Vector2.right;
+
     private void Awake()
     {
         for (int row = 0; row < this.rows; row++)
@@ -28,5 +33,42 @@ public class Invaders : MonoBehaviour
                 invader.transform.localPosition = position;
             }
         }
+    }
+
+    private void Update()
+    {
+        this.transform.position += _direction * this.speed * Time.deltaTime;
+
+        // Obtaining the worldspace cooridnates for left and right edges of the game screen
+        Vector3 leftEdge = Camera.main.ViewportToWorldPoint(Vector3.zero);
+        Vector3 rightEdge = Camera.main.ViewportToWorldPoint(Vector3.right);
+
+        // Looping through each invader to see if it has reached the end of the screen 
+        // if so we will flip the direciton
+        foreach (Transform invader in this.transform)
+        {
+            // Checking if the invader is alive / active
+            if (!invader.gameObject.activeInHierarchy) {
+                continue;
+            }
+
+            // Checking if the invader has hit the right edge of the screen
+            if (_direction == Vector3.right && invader.position.x >= (rightEdge.x - 1.0f)) {
+                AdvanceRow();
+            } else if (_direction == Vector3.left && invader.position.x <= (leftEdge.x + 1.0f)) {
+                AdvanceRow();
+            }
+        }
+    }
+
+    private void AdvanceRow()
+    {
+        // Flipping direction value
+        _direction.x *= -1.0f;
+
+        // Moving row position down 1
+        Vector3 position = this.transform.position;
+        position.y -= 1.0f;
+        this.transform.position = position;
     }
 }
