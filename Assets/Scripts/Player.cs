aa1e9a3d57a25087596cae4f7b20d1996568ac53
Daniel O'Brien <975241@swansea.ac.uk>
2022-02-12 17:12:52 +0000

    Laser Shooting
    - Creating a base projectile class to handle typical behaviours of projectiles.
    - Adding in ability to shoot a laser when space or left mouse is clicked, and only one can be shot at a time.
    - Creating layers for each game object type and handling the physics of how and if certain objects should collide.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,8 +2,11 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public Projectile laserPrefab;
+
     // Setting player speed
     public float speed = 5.0f;
+    private bool _laserActive;
 
     private void Update()
     {
@@ -13,6 +16,30 @@ public class Player : MonoBehaviour
         } else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
             this.transform.position += Vector3.right * this.speed * Time.deltaTime;
         }
+
+        // Call function to shoot if space key is pressed or left mouse button is clicked
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)) {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        // Only shoot if there is currently not an active laser on the game
+        if (!_laserActive) 
+        {
+            // Creatr projectile
+            Projectile projectile = Instantiate(this.laserPrefab, this.transform.position, Quaternion.identity);
+            // Subscribe to destroyed event in order to call a function whenever it occurs
+            projectile.destroyed += LaserDestroyed;
+            _laserActive = true;
+        }
+       
+    }
+
+    private void LaserDestroyed()
+    {
+        _laserActive = false;
     }
    
 }
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    // Setting direciton and speed of projectile
+    public Vector3 direction;
+    public float speed;
+    public System.Action destroyed;
+
+    private void Update()
+    {
+        this.transform.position += this.direction * this.speed * Time.deltaTime;
+    }
+
+    // This function will be called whenever the laser collides with anything, 
+    // as a result of the box collider component added to the laser prefab
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // A delegate to be invoked by another scipt whenever a collision occurs
+        this.destroyed.Invoke();
+        // Destory laser object
+        Destroy(this.gameObject);
+    }
+}
+
